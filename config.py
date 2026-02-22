a421a0abb5c8cda70a118f3d263dcb002f7db6f9
jiohslive <yashpisal8353@gmail.com>
2026-02-23 01:59:20 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-MONGO_URI = os.getenv("MONGO_URI")
-FORCE_SUB_CHANNEL = os.getenv("FORCE_SUB_CHANNEL", "").replace("@", "")
