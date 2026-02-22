a936b46fbe078480077702c0bb9a4be4748a7322
jiohslive <yashpisal8353@gmail.com>
2026-02-23 03:09:43 +0530

    Update file_id_logger.py

--- utils/file_id_logger.py
+++ utils/file_id_logger.py
@@ -1,38 +1,12 @@
-from telegram import Update
-from telegram.constants import ParseMode
-from telegram.ext import ContextTypes
-
-async def file_id_logger(update: Update, context: ContextTypes.DEFAULT_TYPE, ADMIN_ID: int):
-    if update.effective_user.id != ADMIN_ID:
+async def file_id_logger(update, context, ADMIN_ID):
+    msg = update.message
+    if not msg:
         return
 
-    msg = update.effective_message
-
-    file_id = None
-    file_type = None
-
-    if msg.video:
-        file_id = msg.video.file_id
-        file_type = "VIDEO"
-    elif msg.document:
-        file_id = msg.document.file_id
-        file_type = "DOCUMENT"
-    elif msg.audio:
-        file_id = msg.audio.file_id
-        file_type = "AUDIO"
-    elif msg.voice:
-        file_id = msg.voice.file_id
-        file_type = "VOICE"
-    elif msg.photo:
-        file_id = msg.photo[-1].file_id
-        file_type = "PHOTO"
-
-    if file_id:
-        text = (
-            f"📦 <b>File Type:</b> {file_type}\n\n"
-            f"🆔 <b>File ID:</b>\n"
-            f"<code>{file_id}</code>\n\n"
-            f"👉 Copy this file_id and use in /add command"
+    if msg.video or msg.document:
+        file = msg.video or msg.document
+        await context.bot.send_message(
+            ADMIN_ID,
+            f"📁 File ID:\n<code>{file.file_id}</code>",
+            parse_mode="HTML"
         )
-        await msg.reply_text(text, parse_mode=ParseMode.HTML)
-      
