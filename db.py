16ab9c265810c41ebc7e6729d00ccfeace764e3c
jiohslive <yashpisal8353@gmail.com>
2026-02-23 03:08:53 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,12 +1,23 @@
-import os
 from pymongo import MongoClient
+import os
 
-MONGO_URI = os.getenv("MONGO_URL")
-if not MONGO_URI:
-    raise ValueError("MONGO_URL env missing!")
-
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
-db = client["moviebot"]
 
+db = client["rivo_bots"]
 movies_col = db["movies"]
 users_col = db["users"]
+
+# Init Bigg Boss Marathi if not exists
+def init_show():
+    if not movies_col.find_one({"show": "Bigg Boss Marathi"}):
+        movies_col.insert_one({
+            "show": "Bigg Boss Marathi",
+            "seasons": {
+                "6": {
+                    "episodes": {}  # "105": [{"quality": "1080p", "file_id": "..."}]
+                }
+            }
+        })
+
+init_show()
