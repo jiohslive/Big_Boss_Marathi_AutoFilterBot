bde832d4c5fb4b4475bf91b711c3a67b755416a3
jiohslive <yashpisal8353@gmail.com>
2026-02-23 01:37:11 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,11 @@
+import os
 from pymongo import MongoClient
-from config import MONGO_URI
+
+MONGO_URI = os.getenv("MONGO_URL")
+
+if not MONGO_URI:
+    raise ValueError("MONGO_URL is not set in env")
 
 mongo = MongoClient(MONGO_URI)
-db = mongo["moviebot"]
+db = mongo["autofilter"]
 movies_col = db["movies"]
